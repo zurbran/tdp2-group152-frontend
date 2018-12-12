0d7a42ffd0643e518a8f44b21b3d1a055c50504d
Brandon <brandon.russell.961@gmail.com>
2018-12-11 23:04:01 -0300

    Changed project artifact

---
+++ src/main/java/com/tdp2/group152/ui/HomeController.java
@@ -0,0 +1,13 @@
+package com.tdp2.group152.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index.html";
+    }
+}
---
+++ src/main/java/com/tdp2/group152/ui/SpringBootWebApplicationBootstrapJqueryApplication.java
@@ -0,0 +1,12 @@
+package com.tdp2.group152.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplicationBootstrapJqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplicationBootstrapJqueryApplication.class, args);
+	}
+}
